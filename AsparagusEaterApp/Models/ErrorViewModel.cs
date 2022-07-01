1fa3d5d1b4803d064e2a88df7ff4426625ecc04a
Andrey <morgunof-andrey@yandex.ru>
2022-07-01 14:31:31 +0400

    Initial

---
+++ AsparagusEaterApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AsparagusEaterApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
